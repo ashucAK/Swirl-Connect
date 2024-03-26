ed00e1715ac1ad5d1656631a46cccfb3c7fe2f84
ashucAK <singhashishk12@gmail.com>
2024-03-26 16:36:12 +0530

    ....

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { Outlet, Navigate, Route, useLocation } from "react-router-dom";
-
+import {Home, Login, Profile, Register, ResetPassword} from "./pages";
 function Layout(){
   const user = null;
   const location = useLocation()
   return user?.token ? (
     <Outlet />
   ) : (
-    <Navigate to="/login" state={{from:location}} replace />
+    <Navigate to="/login" state={{ from:location }} replace />
   );
 }
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div className="w-full min-h-[100vh]">
+      <Routes>
+        <Route element = {<Layout/>}>
+          <Route path="/" element={<Home />} />
+          <Route path ='/profile/:id?' element = {<Profile />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path ='/login' element = {<Login />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </div>
   );
 }
