84ee6972745c2eb78ef5d7a131a2a33d2052e316
ashucAK <singhashishk12@gmail.com>
2024-03-26 16:19:34 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import { Outlet, Navigate, Route, useLocation } from "react-router-dom";
+
+function Layout(){
+  const user = null;
+  const location = useLocation()
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{from:location}} replace />
+  );
+}
 function App() {
   return (
     <div className="App">
