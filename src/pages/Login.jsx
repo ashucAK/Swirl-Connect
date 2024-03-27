ec21393c96c8d6d7fee7dc8ff10ed2334f9a37a4
ashucAK <singhashishk12@gmail.com>
2024-03-27 23:55:36 +0530

    ......

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import {TbSocial} from "react-icons/tb";
+import React from 'react';
+import {useForm} from "react-hook-form";
+import TextInput from '../components/TextInput';
+import { AiOutlineCodepenCircle } from "react-icons/ai";
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    mode: "onChange",
+  });
   return (
     <div className='bg-bgcolor w-full h-[100vh] flex items-center justify-center p-6'>
-      <div className='w-full md:w-2/3'
-      
+      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
+        <div className='w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
+          <div className='w-full flex gap-2 items-center mb-6'>
+            <div className='p-2 bg-[#0a0b0b] roundede text-white'>
+              <AiOutlineCodepenCircle />
+            </div>
+            <span className='text-2xl text-[#aaaaaa]' font-semibold>
+              Swirl Connect
+            </span>
+          </div>
+          <p className='text-ascent-1 text-base font-semibold'>
+            Log in to your account
+          </p>
+          <span className='text-sm mt-2 text-ascent-2'>
+            Welcome Back
+          </span>
+          <form className='py-8 flex flex-col gap-5'>
+            <TextInput 
+            name = 'email'
+            placeholder = 'email@example.com'
+            label='Email Address'
+            register={register("email", {
+              required: "Email Address is required",
+            })}
+            styles = 'w-full rounded-full'
+            labelStyle = 'ml-2'
+            error={errors.email ? errors.email.message : ""}
+            />
+            <TextInput 
+            name = 'password'
+            placeholder = 'Password'
+            label='Password'
+            type='password'
+            register={register("password", {
+              required: "Password is required",
+            })}
+            styles = 'w-full rounded-full'
+            labelStyle = 'ml-2'
+            error={errors.password ? errors.password?.message : ""}
+            />
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
