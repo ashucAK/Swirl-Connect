553e3ee39f0e43cc8755a0ee7c33d0dae179f93b
ashucAK <singhashishk12@gmail.com>
2024-03-30 15:47:55 +0530

    .....

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { TextInput } from "../components";
-import Loading from '../components/Loading';
-import CustomButton from '../components/CustomButton';
+import { CustomButton, Loading, TextInput } from "../components";
 
 const ResetPassword = () => {
   const [errMsg, setErrMsg] = useState("");
@@ -11,55 +9,58 @@ const ResetPassword = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors }, // Ensure this is correctly assigned
+
+    formState: { errors },
   } = useForm({
     mode: "onChange",
   });
 
-  const onSubmit = async (data) => {
-    // Handle form submission here
-  };
+  const onSubmit = async (data) => {};
 
   return (
-    <div className='w-full h-[100vh] bg-bgcolor flex items-center justify-center p-6'>
+    <div className='w-full h-[100vh] bg-bgColor flex items-center justify-center p-6'>
       <div className='bg-primary w-full md:w-1/3 2xl:w-1/4 px-6 py-8 shadow-md rounded-lg'>
         <p className='text-ascent-1 text-lg font-semibold'>Email Address</p>
+
         <span className='text-sm text-ascent-2'>
           Enter email address used during registration
         </span>
+
         <form
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onsubmit)}
           className='py-4 flex flex-col gap-5'
         >
-          <TextInput 
+          <TextInput
             name='email'
             placeholder='email@example.com'
-            label='Email Address'
+            type='email'
             register={register("email", {
-              required: "Email Address is required",
+              required: "Email Address is required!",
             })}
-            styles='w-full rounded-full'
+            styles='w-full rounded-lg'
             labelStyle='ml-2'
             error={errors.email ? errors.email.message : ""}
           />
           {errMsg?.message && (
-            <span className={`text-sm ${
-              errMsg?.status == "failed" 
-                ? "text-[#f64949fe]"
-                : "text-[#2ba150fe]"
-            } mt-0.5`}>
+            <span
+              role='alert'
+              className={`text-sm ${
+                errMsg?.status === "failed"
+                  ? "text-[#f64949fe]"
+                  : "text-[#2ba150fe]"
+              } mt-0.5`}
+            >
               {errMsg?.message}
             </span>
           )}
+
           {isSubmitting ? (
-            <Loading/>
+            <Loading />
           ) : (
             <CustomButton
               type='submit'
-              containStyles={`inline-flex justify-center rounded-md
-              bg-blue px-8 py-3 text-sm font-medium text-white
-              outline-none`}
-              title='Login'
+              containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+              title='Submit'
             />
           )}
         </form>
--- src/redux/postSlice.js
+++ src/redux/postSlice.js
@@ -1,24 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { dispatch } from "./store";
 
 const initialState = {
-    posts: {},
+  posts: {},
 };
 
 const postSlice = createSlice({
-    name: "post",
-    initialState,
-    reducers: {
-        getPosts(state, action){
-            state.posts = action.payload;
-        },
+  name: "post",
+  initialState,
+  reducers: {
+    getPosts(state, action) {
+      state.posts = action.payload;
     },
+  },
 });
 
 export default postSlice.reducer;
 
-export function SetPosts(post){
-    return (dispatch, getState) => {
-        dispatch(postSlice.actions.getPosts(post));
-    };
+export function SetPosts(post) {
+  return (dispatch, getState) => {
+    dispatch(postSlice.actions.getPosts(post));
+  };
 }
