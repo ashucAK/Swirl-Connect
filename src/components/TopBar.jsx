db4740c7d609ecd3427d0456897d746a37b947a1
ashucAK <singhashishk12@gmail.com>
2024-04-02 13:43:41 +0530

    ....

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -35,7 +35,7 @@ const TopBar = () => {
           <DiSublime />
         </div>
         <span className='text-xl md:text-2xl text-[#065ad8] font-semibold'>
-          ShareFun
+          Swirl Connect
         </span>
       </Link>
 
