b2f76856eff76fc27f6b21077ce275fbda044ac6
ashucAK <singhashishk12@gmail.com>
2024-04-07 21:44:19 +0530

    ....

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import { DiSublime } from "react-icons/di";
-import { BsShare } from "react-icons/bs";
-import { AiOutlineInteraction } from "react-icons/ai";
-import { ImConnection } from "react-icons/im";
 import { CustomButton, Loading, TextInput } from "../components";
-import { BgImage } from "../assets";
 
 const Login = () => {
   const {
@@ -25,110 +21,89 @@ const Login = () => {
   const dispatch = useDispatch();
   return (
     <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-6'>
-      <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
-        {/* LEFT */}
-        <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center '>
-          <div className='w-full flex gap-2 items-center mb-6'>
-            <div className='p-2 bg-[#76787a] rounded text-white'>
-              <DiSublime />
-            </div>
-            <span className='text-2xl text-[#b1b4b9] font-semibold'>
-              Swirl Connect
-            </span>
+      <div className='w-full lg:w-1/2 h-full p-10 flex flex-col justify-center'>
+        <div className='w-full flex gap-2 items-center mb-6'>
+          <div className='p-2 bg-[#76787a] rounded text-white'>
+            <DiSublime />
           </div>
+          <span className='text-2xl text-[#b1b4b9] font-semibold'>
+            Swirl Connect
+          </span>
+        </div>
 
-          <p className='text-ascent-1 text-lg font-bold font-serif'>
-            Sign in to SC
-          </p>
+        <p className='text-ascent-1 text-lg font-bold font-serif'>
+          Sign in to SC
+        </p>
 
-          <form
-            className='py-8 flex flex-col gap-5='
-            onSubmit={handleSubmit(onSubmit)}
+        <form
+          className='py-8 flex flex-col gap-5='
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <TextInput
+            name='email'
+            placeholder='email@example.com'
+            label='Email Address'
+            type='email'
+            register={register("email", {
+              required: "Email Address is required",
+            })}
+            styles='w-full rounded-full'
+            labelStyle='ml-2'
+            error={errors.email ? errors.email.message : ""}
+          />
+
+          <TextInput
+            name='password'
+            label='Password'
+            placeholder='Password'
+            type='password'
+            styles='w-full rounded-full'
+            labelStyle='ml-2'
+            register={register("password", {
+              required: "Password is required!",
+            })}
+            error={errors.password ? errors.password?.message : ""}
+          />
+
+          <Link
+            to='/reset-password'
+            className='text-sm text-right text-[#b1b4b9] font-semibold'
           >
-            <TextInput
-              name='email'
-              placeholder='email@example.com'
-              label='Email Address'
-              type='email'
-              register={register("email", {
-                required: "Email Address is required",
-              })}
-              styles='w-full rounded-full'
-              labelStyle='ml-2'
-              error={errors.email ? errors.email.message : ""}
-            />
+            Forgot Password ?
+          </Link>
 
-            <TextInput
-              name='password'
-              label='Password'
-              placeholder='Password'
-              type='password'
-              styles='w-full rounded-full'
-              labelStyle='ml-2'
-              register={register("password", {
-                required: "Password is required!",
-              })}
-              error={errors.password ? errors.password?.message : ""}
-            />
-
-            <Link
-              to='/reset-password'
-              className='text-sm text-right text-[#b1b4b9] font-semibold'
+          {errMsg?.message && (
+            <span
+              className={`text-sm ${
+                errMsg?.status == "failed"
+                  ? "text-[#f64949fe]"
+                  : "text-[#2ba150fe]"
+              } mt-0.5`}
             >
-              Forgot Password ?
-            </Link>
-
-            {errMsg?.message && (
-              <span
-                className={`text-sm ${
-                  errMsg?.status == "failed"
-                    ? "text-[#f64949fe]"
-                    : "text-[#2ba150fe]"
-                } mt-0.5`}
-              >
-                {errMsg?.message}
-              </span>
-            )}
-
-            {isSubmitting ? (
-              <Loading />
-            ) : (
-              <CustomButton
-                type='submit'
-                containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
-                title='Login'
-              />
-            )}
-          </form>
-
-          <p className='text-ascent-2 text-sm text-center'>
-            Don't have an account?
-            <Link
-              to='/register'
-              className='text-[#b1b4b9] font-semibold ml-2 cursor-pointer'
-            >
-              Create Account
-            </Link>
-          </p>
-        </div>
-        <div className='hidden w-1/2 h-full lg:flex flex-col items-center justify-center bg-[#76787a]'>
-          <div className='relative w-full flex items-center justify-center'>
-            <img
-              src={BgImage}
-              alt='Bg Image'
-              className='w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover'
-            />
-          </div>
-
-          <div className='mt-16 text-center'>
-            <p className='text-white text-base'>
-              Make connections with friends and share the joy!
-            </p>
-            <span className='text-sm text-white/80'>
-              Share memorable moments with friends and beyond.
+              {errMsg?.message}
             </span>
-          </div>
-        </div>
+          )}
+
+          {isSubmitting ? (
+            <Loading />
+          ) : (
+            <CustomButton
+              type='submit'
+              containerStyles={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+              title='Login'
+            />
+          )}
+        </form>
+
+        <p className='text-ascent-2 text-sm text-center'>
+          Don't have an account?
+          <Link
+            to='/register'
+            className='text-[#b1b4b9] font-semibold ml-2 cursor-pointer'
+          >
+            Create Account
+          </Link>
+        </p>
       </div>
     </div>
   );
